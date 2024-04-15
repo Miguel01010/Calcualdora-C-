3f1388f261518813127e95f35831c9676e65f35c
primerpasoGit <miguelchauxs@gmail.com>
2024-04-15 11:54:10 -0500

    Proyecto calculadora

---
+++ calculadora/CALCULADORA.Designer.cs
@@ -0,0 +1,341 @@
+﻿
+namespace calculadora
+{
+    partial class CALCULADORA
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnN10 = new Button();
+            btnN7 = new Button();
+            btnN4 = new Button();
+            btnN0 = new Button();
+            button5 = new Button();
+            btnN2 = new Button();
+            btnN5 = new Button();
+            btnN8 = new Button();
+            btnN11 = new Button();
+            btnN17 = new Button();
+            btnN3 = new Button();
+            btnN6 = new Button();
+            btnN9 = new Button();
+            btnN12 = new Button();
+            btnN16 = new Button();
+            btnN15 = new Button();
+            btnN14 = new Button();
+            btnN13 = new Button();
+            tbxScreen = new TextBox();
+            SuspendLayout();
+            // 
+            // btnN10
+            // 
+            btnN10.BackColor = SystemColors.HighlightText;
+            btnN10.Font = new Font("Corbel", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnN10.Location = new Point(9, 59);
+            btnN10.Name = "btnN10";
+            btnN10.Size = new Size(61, 53);
+            btnN10.TabIndex = 0;
+            btnN10.Text = "+";
+            btnN10.UseVisualStyleBackColor = false;
+            btnN10.Click += btnN10_Click;
+            // 
+            // btnN7
+            // 
+            btnN7.BackColor = SystemColors.HighlightText;
+            btnN7.Font = new Font("Corbel", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnN7.Location = new Point(9, 118);
+            btnN7.Name = "btnN7";
+            btnN7.Size = new Size(61, 53);
+            btnN7.TabIndex = 1;
+            btnN7.Text = "7";
+            btnN7.UseVisualStyleBackColor = false;
+            btnN7.Click += btnN7_Click;
+            // 
+            // btnN4
+            // 
+            btnN4.BackColor = SystemColors.HighlightText;
+            btnN4.Font = new Font("Corbel", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnN4.Location = new Point(9, 177);
+            btnN4.Name = "btnN4";
+            btnN4.Size = new Size(61, 53);
+            btnN4.TabIndex = 2;
+            btnN4.Text = "4";
+            btnN4.UseVisualStyleBackColor = false;
+            btnN4.Click += btnN4_Click;
+            // 
+            // btnN0
+            // 
+            btnN0.BackColor = SystemColors.HighlightText;
+            btnN0.Font = new Font("Corbel", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnN0.Location = new Point(9, 295);
+            btnN0.Name = "btnN0";
+            btnN0.Size = new Size(128, 53);
+            btnN0.TabIndex = 4;
+            btnN0.Text = " 0";
+            btnN0.UseVisualStyleBackColor = false;
+            btnN0.Click += btnN0_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.HighlightText;
+            button5.Font = new Font("Corbel", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.Location = new Point(9, 236);
+            button5.Name = "button5";
+            button5.Size = new Size(61, 53);
+            button5.TabIndex = 3;
+            button5.Text = "1";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // btnN2
+            // 
+            btnN2.BackColor = SystemColors.HighlightText;
+            btnN2.Font = new Font("Corbel", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnN2.Location = new Point(76, 236);
+            btnN2.Name = "btnN2";
+            btnN2.Size = new Size(61, 53);
+            btnN2.TabIndex = 8;
+            btnN2.Text = "2";
+            btnN2.UseVisualStyleBackColor = false;
+            btnN2.Click += btnN2_Click;
+            // 
+            // btnN5
+            // 
+            btnN5.BackColor = SystemColors.HighlightText;
+            btnN5.Font = new Font("Corbel", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnN5.Location = new Point(76, 177);
+            btnN5.Name = "btnN5";
+            btnN5.Size = new Size(61, 53);
+            btnN5.TabIndex = 7;
+            btnN5.Text = "5";
+            btnN5.UseVisualStyleBackColor = false;
+            btnN5.Click += btnN5_Click;
+            // 
+            // btnN8
+            // 
+            btnN8.BackColor = SystemColors.HighlightText;
+            btnN8.Font = new Font("Corbel", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnN8.Location = new Point(76, 118);
+            btnN8.Name = "btnN8";
+            btnN8.Size = new Size(61, 53);
+            btnN8.TabIndex = 6;
+            btnN8.Text = "8";
+            btnN8.UseVisualStyleBackColor = false;
+            btnN8.Click += btnN8_Click;
+            // 
+            // btnN11
+            // 
+            btnN11.BackColor = SystemColors.HighlightText;
+            btnN11.Font = new Font("Corbel", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnN11.Location = new Point(76, 59);
+            btnN11.Name = "btnN11";
+            btnN11.Size = new Size(61, 53);
+            btnN11.TabIndex = 5;
+            btnN11.Text = "-";
+            btnN11.UseVisualStyleBackColor = false;
+            btnN11.Click += button10_Click;
+            // 
+            // btnN17
+            // 
+            btnN17.BackColor = SystemColors.HighlightText;
+            btnN17.Font = new Font("Corbel", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnN17.Location = new Point(143, 295);
+            btnN17.Name = "btnN17";
+            btnN17.Size = new Size(61, 53);
+            btnN17.TabIndex = 14;
+            btnN17.Text = ".";
+            btnN17.UseVisualStyleBackColor = false;
+            btnN17.Click += btnN17_Click;
+            // 
+            // btnN3
+            // 
+            btnN3.BackColor = SystemColors.HighlightText;
+            btnN3.Font = new Font("Corbel", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnN3.Location = new Point(143, 236);
+            btnN3.Name = "btnN3";
+            btnN3.Size = new Size(61, 53);
+            btnN3.TabIndex = 13;
+            btnN3.Text = "3";
+            btnN3.UseVisualStyleBackColor = false;
+            btnN3.Click += btnN3_Click;
+            // 
+            // btnN6
+            // 
+            btnN6.BackColor = SystemColors.HighlightText;
+            btnN6.Font = new Font("Corbel", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnN6.Location = new Point(143, 177);
+            btnN6.Name = "btnN6";
+            btnN6.Size = new Size(61, 53);
+            btnN6.TabIndex = 12;
+            btnN6.Text = "6";
+            btnN6.UseVisualStyleBackColor = false;
+            btnN6.Click += btnN6_Click;
+            // 
+            // btnN9
+            // 
+            btnN9.BackColor = SystemColors.HighlightText;
+            btnN9.Font = new Font("Corbel", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnN9.Location = new Point(143, 118);
+            btnN9.Name = "btnN9";
+            btnN9.Size = new Size(61, 53);
+            btnN9.TabIndex = 11;
+            btnN9.Text = "9";
+            btnN9.UseVisualStyleBackColor = false;
+            btnN9.Click += btnN9_Click;
+            // 
+            // btnN12
+            // 
+            btnN12.BackColor = SystemColors.HighlightText;
+            btnN12.Font = new Font("Corbel", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnN12.Location = new Point(143, 59);
+            btnN12.Name = "btnN12";
+            btnN12.Size = new Size(61, 53);
+            btnN12.TabIndex = 10;
+            btnN12.Text = "x";
+            btnN12.UseVisualStyleBackColor = false;
+            btnN12.Click += btnN12_Click;
+            // 
+            // btnN16
+            // 
+            btnN16.BackColor = SystemColors.HighlightText;
+            btnN16.Font = new Font("Corbel", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnN16.Location = new Point(210, 236);
+            btnN16.Name = "btnN16";
+            btnN16.Size = new Size(61, 112);
+            btnN16.TabIndex = 19;
+            btnN16.Text = "=";
+            btnN16.UseVisualStyleBackColor = false;
+            btnN16.Click += btnN16_Click;
+            // 
+            // btnN15
+            // 
+            btnN15.BackColor = SystemColors.HighlightText;
+            btnN15.Font = new Font("Corbel", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnN15.Location = new Point(210, 177);
+            btnN15.Name = "btnN15";
+            btnN15.Size = new Size(61, 53);
+            btnN15.TabIndex = 17;
+            btnN15.Text = " ←";
+            btnN15.UseVisualStyleBackColor = false;
+            btnN15.Click += btnN15_Click;
+            // 
+            // btnN14
+            // 
+            btnN14.BackColor = SystemColors.HighlightText;
+            btnN14.Font = new Font("Corbel", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnN14.Location = new Point(210, 118);
+            btnN14.Name = "btnN14";
+            btnN14.Size = new Size(61, 53);
+            btnN14.TabIndex = 16;
+            btnN14.Text = "CE";
+            btnN14.UseVisualStyleBackColor = false;
+            btnN14.Click += btnN14_Click;
+            // 
+            // btnN13
+            // 
+            btnN13.BackColor = SystemColors.HighlightText;
+            btnN13.Font = new Font("Corbel", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnN13.Location = new Point(210, 59);
+            btnN13.Name = "btnN13";
+            btnN13.Size = new Size(61, 53);
+            btnN13.TabIndex = 15;
+            btnN13.Text = "/";
+            btnN13.UseVisualStyleBackColor = false;
+            btnN13.Click += button20_Click;
+            // 
+            // tbxScreen
+            // 
+            tbxScreen.BackColor = SystemColors.ButtonShadow;
+            tbxScreen.Font = new Font("Akhbar MT", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            tbxScreen.ForeColor = Color.Black;
+            tbxScreen.Location = new Point(12, 12);
+            tbxScreen.Name = "tbxScreen";
+            tbxScreen.ReadOnly = true;
+            tbxScreen.Size = new Size(259, 41);
+            tbxScreen.TabIndex = 20;
+            tbxScreen.Text = "0";
+            tbxScreen.TextAlign = HorizontalAlignment.Right;
+            tbxScreen.TextChanged += textBox1_TextChanged;
+            // 
+            // CALCULADORA
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(512, 357);
+            Controls.Add(tbxScreen);
+            Controls.Add(btnN16);
+            Controls.Add(btnN15);
+            Controls.Add(btnN14);
+            Controls.Add(btnN13);
+            Controls.Add(btnN17);
+            Controls.Add(btnN3);
+            Controls.Add(btnN6);
+            Controls.Add(btnN9);
+            Controls.Add(btnN12);
+            Controls.Add(btnN2);
+            Controls.Add(btnN5);
+            Controls.Add(btnN8);
+            Controls.Add(btnN11);
+            Controls.Add(btnN0);
+            Controls.Add(button5);
+            Controls.Add(btnN4);
+            Controls.Add(btnN7);
+            Controls.Add(btnN10);
+            ForeColor = SystemColors.MenuText;
+            Name = "CALCULADORA";
+            Text = "Form1";
+            TransparencyKey = Color.Plum;
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+
+
+        #endregion
+
+        private Button btnN10;
+        private Button btnN7;
+        private Button btnN4;
+        private Button btnN0;
+        private Button button5;
+        private Button btnN2;
+        private Button btnN5;
+        private Button btnN8;
+        private Button btnN11;
+        private Button btnN17;
+        private Button btnN3;
+        private Button btnN6;
+        private Button btnN9;
+        private Button btnN12;
+        private Button btnN16;
+        private Button btnN15;
+        private Button btnN14;
+        private Button btnN13;
+        private TextBox tbxScreen;
+    }
+}
---
+++ calculadora/CALCULADORA.cs
@@ -0,0 +1,252 @@
+namespace calculadora
+{
+    public partial class CALCULADORA : Form
+    {
+
+        double primero;
+        double segundo;
+        string operador;
+        public CALCULADORA()
+        {
+            InitializeComponent();
+        }
+
+        clase.suma obj = new clase.suma();
+        clase.resta obj2 = new clase.resta();
+        clase.multiplicacion obj3 = new clase.multiplicacion();
+        clase.divicion obj4 = new clase.divicion();
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            operador = "-";
+            primero = double.Parse(tbxScreen.Text);
+            tbxScreen.Clear();
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            operador = "/";
+            primero = double.Parse(tbxScreen.Text);
+            tbxScreen.Clear();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (tbxScreen.Text >= 2 && tbxScreen(texto[texto.Length - 2]) && tbxScreen q(texto[texto.Length - 1]))
+            {
+                return "1";
+            }
+
+        }
+
+        private void btnN0_Click(object sender, EventArgs e)
+        {
+           
+            if (tbxScreen.Text == "0")
+            {
+                return;
+
+            }
+
+            else
+            {
+                tbxScreen.Text += "0";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if(tbxScreen.Text == "0" && tbxScreen.Text != null)
+            {
+                tbxScreen.Text = "1";
+            }
+            else
+            {
+             tbxScreen.Text +="1";
+
+            }
+        }
+
+        private void btnN2_Click(object sender, EventArgs e)
+
+        {
+            if (tbxScreen.Text == "0" && tbxScreen.Text != null)
+            {
+                tbxScreen.Text = "2";
+            }
+            else
+            {
+                tbxScreen.Text += "2";
+            }
+        }
+
+private void btnN3_Click(object sender, EventArgs e)
+        {
+
+            {
+                if (tbxScreen.Text == "0" && tbxScreen.Text != null)
+                {
+                    tbxScreen.Text = "3";
+                }
+                else
+                {
+                    tbxScreen.Text += "3";
+                }
+            }
+        }
+
+        private void btnN4_Click(object sender, EventArgs e)
+        {
+            if (tbxScreen.Text == "0" && tbxScreen.Text != null)
+            {
+                tbxScreen.Text = "4";
+            }
+            else
+            {
+                tbxScreen.Text += "4";
+            }
+        }
+
+        private void btnN5_Click(object sender, EventArgs e)
+        {
+            if (tbxScreen.Text == "0" && tbxScreen.Text != null)
+            {
+                tbxScreen.Text = "5";
+            }
+            else
+            {
+                tbxScreen.Text += "5";
+            }
+        }
+
+        private void btnN6_Click(object sender, EventArgs e)
+        {
+            if (tbxScreen.Text == "0" && tbxScreen.Text != null)
+            {
+                tbxScreen.Text = "6";
+            }
+            else
+            {
+                tbxScreen.Text += "6";
+            }
+        }
+
+        private void btnN7_Click(object sender, EventArgs e)
+        {
+            if (tbxScreen.Text == "0" && tbxScreen.Text != null)
+            {
+                tbxScreen.Text = "7";
+            }
+            else
+            {
+                tbxScreen.Text += "7";
+            }
+        }
+
+        private void btnN8_Click(object sender, EventArgs e)
+        {
+            if (tbxScreen.Text == "0" && tbxScreen.Text != null)
+            {
+                tbxScreen.Text = "8";
+            }
+            else
+            {
+                tbxScreen.Text += "8";
+            }
+        }
+
+        private void btnN9_Click(object sender, EventArgs e)
+        {
+            if (tbxScreen.Text == "0" && tbxScreen.Text != null)
+            {
+                tbxScreen.Text = "9";
+            }
+            else
+            {
+                tbxScreen.Text += "9";
+            }
+        }
+
+        private void btnN17_Click(object sender, EventArgs e)
+        {
+            tbxScreen.Text = tbxScreen.Text + ".";
+        }
+
+        private void btnN10_Click(object sender, EventArgs e)
+        {
+            operador = "+";
+            primero = double.Parse(tbxScreen.Text);
+            tbxScreen.Clear();
+        }
+
+        private void btnN12_Click(object sender, EventArgs e)
+        {
+
+            operador = "*";
+            primero = double.Parse(tbxScreen.Text);
+            tbxScreen.Clear();
+        }
+
+
+        private void btnN16_Click(object sender, EventArgs e)
+        {
+            segundo = double.Parse(tbxScreen.Text);
+
+            double Sum;
+            double Rest;
+            double Mul;
+            double Div;
+
+            switch (operador)
+            {
+                case "+":
+                    Sum = obj.Sumar((primero), (segundo));
+                    tbxScreen.Text = Sum.ToString();
+                    break;
+
+                case "-":
+                    Rest = obj2.Restar((primero), (segundo));
+                    tbxScreen.Text = Rest.ToString();
+                    break;
+
+                case "*":
+                    Mul = obj3.Multiplicar((primero), (segundo));
+                    tbxScreen.Text = Mul.ToString();
+                    break;
+
+                case "/":
+                    Div = obj4.Divicion((primero), (segundo));
+                    tbxScreen.Text = Div.ToString();
+                    break;
+
+
+
+            }
+        }
+
+        private void btnN14_Click(object sender, EventArgs e)
+        {
+            tbxScreen.Clear();
+        }
+
+        private void btnN15_Click(object sender, EventArgs e)
+        {
+            if (tbxScreen.Text.Length == 1)
+                tbxScreen.Text = "";
+            else
+                tbxScreen.Text = tbxScreen.Text.Substring(0, tbxScreen.Text.Length - 1);
+        }
+    }
+}
